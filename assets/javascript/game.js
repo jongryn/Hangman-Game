0c8b805de30c50da5daca27bcbce5ffeb0800ddd
Jonathan Gryn <jonathangryn@Jonathans-MacBook-Pro.local>
2017-07-03 10:20:13 -0700

    added bg image

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,183 +4,133 @@
 // Revisions: Jon (6/26/17) - Added js      
 */
 
-// Self invoking function hides currentWord from the console
-(function() {
-  /*
-   * Pick from alphabet keypad. Returns the letter chosen.
-   */
-  $("#alphabet-keypad").on("click", ".letter-button", pickLetter);
-
-  function pickLetter() {
-    var letterPicked = $(this);
-
-    letterPicked
-      .removeClass("letter-button")
-      .addClass("letter-disabled");
-
-    letterPicked = letterPicked.html();
-    handlePickedLetter(letterPicked);
+// GLOBAL VARIABLES (Accessible by all functions)
+// ==================================================================================================
+// Array of Word Options (all lowercase)
+var wordsList = ["jackjohnson", "rebelution", "dirtyheads", "iration", "sublime", "bobmarley", "benharper", "threeleggedfox", "longbeachduballstars", "barringtonlevy", "damianmarley", "ziggymarley", "pepper", "thegreen", "commonkings", "jboog", "sammyj", "chronixx", "theexpendables", "slightlystoopid", "soja", "shwayze", "steelpulse", "thewailers", "pacificdub", "aloharadio", "war"];
+// Solution will be held here.
+var chosenWord = "";
+// This will break the solution into individual letters to be stored in array.
+var lettersInChosenWord = [];
+// This will be the number of blanks we show based on the solution
+var numBlanks = 0;
+// Holds a mix of blank and solved letters (ex: 'n, _ _, n, _').
+var blanksAndSuccesses = [];
+// Holds all of the wrong guesses
+var wrongGuesses = [];
+// Game counters
+var winCounter = 0;
+var lossCounter = 0;
+var numGuesses = 9;
+// FUNCTIONS (These are bits of code that we will call upon to run when needed)
+// =========================================================================================
+// startGame()
+// Its how we we will start and restart the game.
+// (Note: It's not being run here. It's just being made for future use.)
+function startGame() {
+  // Reset the guesses back to 0.
+  numGuesses = 9;
+  // Solution is chosen randomly from wordList.
+  chosenWord = wordsList[Math.floor(Math.random() * wordsList.length)];
+  // The word is broken into individual letters.
+  lettersInChosenWord = chosenWord.split("");
+  // We count the number of letters in the word.
+  numBlanks = lettersInChosenWord.length;
+  // We print the solution in console (for testing).
+  console.log(chosenWord);
+  // CRITICAL LINE - Here we *reset* the guess and success array at each round.
+  blanksAndSuccesses = [];
+  // CRITICAL LINE - Here we *reset* the wrong guesses from the previous round.
+  wrongGuesses = [];
+  // Fill up the blanksAndSuccesses list with appropriate number of blanks.
+  // This is based on number of letters in solution.
+  for (var i = 0; i < numBlanks; i++) {
+    blanksAndSuccesses.push("_");
   }
-
-  function handlePickedLetter(letterPicked) {
-    var resultMatches = [];
-    var ind = currentWord.indexOf(letterPicked);
-
-    // if letterPicked matches one or more letters in the current word
-    // push all instances of that letter to resultMatches
-    while (ind !== -1) {
-      resultMatches.push(ind);
-      ind = currentWord.indexOf(letterPicked, ind + 1);
-    }
-
-    //if resultMatches is greater than 0 proceed to place them in the dom
-    if (resultMatches.length > 0) {
-      var letterBlocks = document.getElementsByClassName("is-letter");
-      resultMatches.map(function(num) {
-
-        var domElem = document.createElement("span");
-        domElem.innerHTML = currentWordFull[num].toUpperCase();
-        letterBlocks[num].appendChild(domElem);
-        displayCongratulatoryMessageOnWin();
-
-      });
-    //if letterBlock is not greater than 0 put the letter in the graveyard
-    } else {
-      var domElem = document.createElement("div");
-      domElem.className = "grave-letter";
-      domElem.innerHTML = letterPicked;
-      document.getElementById("letter-graveyard").appendChild(domElem);
-      hangmanGraphic.addBodyPart();
-      displayGameOverMessageOnLose();
-    }
-  }
-
-  function displayCongratulatoryMessageOnWin(){
-    var correctlyGuessedLettersCount = $(".is-letter > span").length;
-    if (correctlyGuessedLettersCount === currentWord.length) {
-      $("#congratulatory_message").modal('show');
-    }
-  }
-
-  function displayGameOverMessageOnLose() {
-    var incorrectlyGuessedLettersCount = $("#letter-graveyard > div").length;
-    //If number of letters guessed is equal to maxParts
-    if (incorrectlyGuessedLettersCount === 7 ) {
-      $("#gameover_message").modal('show');
-      var gameOverMessage = "Uh oh. You took too many tries to guess the word. The correct word is - '" + currentWord + "'. Better luck next time.";
-      $(".lead").text(gameOverMessage);
+  // Print the initial blanks in console.
+  console.log(blanksAndSuccesses);
+  // Reprints the guessesLeft to 9
+  document.getElementById("guesses-left").innerHTML = numGuesses;
+  // Prints the blanks at the beginning of each round in the HTML
+  document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
+  // Clears the wrong guesses from the previous round
+  document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+}
+// checkLettesr() function
+// It's where we will do all of the comparisons for matches.
+// Again, it's not being called here. It's just being made for future use.
+function checkLetters(letter) {
+  // This boolean will be toggled based on whether or not a user letter is found anywhere in the word.
+  var letterInWord = false;
+  // Check if a letter exists inside the array at all.
+  for (var i = 0; i < numBlanks; i++) {
+    if (chosenWord[i] === letter) {
+      // If the letter exists then toggle this boolean to true. This will be used in the next step.
+      letterInWord = true;
     }
   }
-
-  /*
-   * Hangman graphic with methods addBodyPart() and reset()
-   */
-  var hangmanGraphic = function () {
-    var bodyParts = 0,
-        maxParts = 7;
-    return {
-      addBodyPart : function () {
-        if (bodyParts < maxParts) {
-          ++bodyParts;
-          $("#hangman-frame" + bodyParts).css("opacity", 1);
-        }
-      },
-
-      reset : function () {
-        $(".hangman-frames").css("opacity", 0);
-        $("#hangman-frame0").css("opacity", 1);
-        bodyParts = 0;
-        resetAlphabetKeypad();
-        removeGraveyardLetters();
-        removeCorrectlyGuessedLetters();
-        removeFillInTheBlanksAroundOldWord();
-        setWordToBeGuessed();
+  // If the letter exists somewhere in the word, then figure out exactly where (which indices).
+  if (letterInWord) {
+    // Loop through the word.
+    for (var j = 0; j < numBlanks; j++) {
+      // Populate the blanksAndSuccesses with every instance of the letter.
+      if (chosenWord[j] === letter) {
+        // Here we set the specific space in blanks and letter equal to the letter when there is a match.
+        blanksAndSuccesses[j] = letter;
       }
-    };
-  }();
-
-  // Next 2 lines will be refactored into interface for
-  //   losing a life and reseting the game
-  $(".reset").on("click", hangmanGraphic.reset);
-
-  function resetAlphabetKeypad(){
-    $("#alphabet-keypad > .letter-disabled").each(function(index, element){
-      $(element).removeClass().addClass('letter-button');
-    });
+    }
+    // Logging for testing.
+    console.log(blanksAndSuccesses);
   }
-
-  function removeGraveyardLetters(){
-    $('#letter-graveyard > div').each(function(index, element){
-      $(element).remove();
-    });
+  // If the letter doesn't exist at all...
+  else {
+    // ..then we add the letter to the list of wrong letters, and we subtract one of the guesses.
+    wrongGuesses.push(letter);
+    numGuesses--;
   }
-
-  function removeCorrectlyGuessedLetters(){
-    $('#word-to-guess').each(function(index, element){
-      $(element).children().html('');
-    });
+}
+// roundComplete() function
+// Here we will have all of the code that needs to be run after each guess is made
+function roundComplete() {
+  // First, log an initial status update in the console telling us how many wins, losses, and guesses are left.
+  console.log("WinCount: " + winCounter + " | LossCount: " + lossCounter + " | NumGuesses: " + numGuesses);
+  // Update the HTML to reflect the new number of guesses. Also update the correct guesses.
+  document.getElementById("guesses-left").innerHTML = numGuesses;
+  // This will print the array of guesses and blanks onto the page.
+  document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
+  // This will print the wrong guesses onto the page.
+  document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+  // If we have gotten all the letters to match the solution...
+  if (lettersInChosenWord.toString() === blanksAndSuccesses.toString()) {
+    // ..add to the win counter & give the user an alert.
+    winCounter++;
+    alert("You win!");
+    // Update the win counter in the HTML & restart the game.
+    document.getElementById("win-counter").innerHTML = winCounter;
+    startGame();
   }
-
-  function removeFillInTheBlanksAroundOldWord(){
-    $("#word-to-guess").html('');
+  // If we've run out of guesses..
+  else if (numGuesses === 0) {
+    // Add to the loss counter.
+    lossCounter++;
+    // Give the user an alert.
+    alert("You lose");
+    // Update the loss counter in the HTML.
+    document.getElementById("loss-counter").innerHTML = lossCounter;
+    // Restart the game.
+    startGame();
   }
-
-  // adding dictionary and word filter //
-  var hangmanWords = [
-    "the","of","and","a","to","in","is","you","that","it","he",
-     "was","for","on","are","as","with","his","they","i","at","be",
-     "this","have","from","or","one","had","by","word","but","not",
-     "what","all","were","we","when","your","can","said","there",
-     "use","an","each","which","she","do","how","their","if","will",
-     "up","other","about","out","many","then","them","these","so",
-     "some","her","would","make","like","him","into","time","has",
-     "look","two","more","write","go","see","number","no","way",
-     "could","people","my","than","first","water","been","call",
-     "who","oil","its","now","find","long","down","day","did","get",
-     "come","made","may","part"
-  ];
-
-  var easyArray = hangmanWords.filter(function(word){
-    return word.length <= 4;
-  });
-
-  var hardArray = hangmanWords.filter(function(word){
-    return word.length > 4;
-  });
-
-  function wordSelect (array) {
-    var num = Math.floor(Math.random() * (array.length - 1));
-    var word = array[num];
-    return word;
-  }
-
-  function setWordToBeGuessed(){
-
-    currentWordFull = wordSelect(hangmanWords);//IMPORTANT: replace the number with wordSelect (the function) for production use
-
-    //set an all upper case version of the current word
-    currentWord = currentWordFull.toUpperCase();
-    //creates blocks in the DOM indicating where there are letters and spaces
-
-
-    currentWord.split("").map(function(character) {
-      var guessWordBlock = document.getElementById("word-to-guess");
-
-      var domElem = document.createElement("div");
-
-      if (character.match(/[a-z]/i)) {
-        domElem.className = "character-block is-letter";
-
-      } else {
-        domElem.className = "character-block";
-      }
-
-      guessWordBlock.appendChild(domElem);
-    });
-  }
-
-  var currentWordFull;
-  var currentWord;
-
-  setWordToBeGuessed();
-})();
+}
+// MAIN PROCESS (THIS IS THE CODE THAT CONTROLS WHAT IS ACTUALLY RUN)
+// ==================================================================================================
+// Starts the Game by running the startGame() function
+startGame();
+// Then initiate the function for capturing key clicks.
+document.onkeyup = function(event) {
+  // Converts all key clicks to lowercase letters.
+  var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+  // Runs the code to check for correctness.
+  checkLetters(letterGuessed);
+  // Runs the code after each round is done.
+  roundComplete();
+};
